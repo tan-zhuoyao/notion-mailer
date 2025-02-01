baeb5991f629084733eb3f5c53cfd383e5713040
Tan Zhuo Yao <e0406711@u.nus.edu>
2025-02-01 22:18:08 +0800

    Reorder logic

--- index.js
+++ index.js
@@ -14,10 +14,6 @@ const notion = new Client({
   auth: process.env.NOTION_SECRET_KEY,
 })
 
-let transporter = await createTransporter();
-
-const scheduler = new Scheduler();
-
 const hostname = '0.0.0.0';
 const port = process.env.PORT || 3000;
 
@@ -31,7 +27,11 @@ server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
 
-if (process.env.NODE_ENV !== "test")
+if (process.env.NODE_ENV !== "test") {
+  let transporter = await createTransporter();
+  const scheduler = new Scheduler();
   poll(notion, transporter, scheduler);
+}
+  
 
 export default server;
